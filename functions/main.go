28f51472ea9478d266b2a63e6586451cd945f227
Prathmesh Adhav <adhavprathmesh972@gmail.com>
2026-08-31 14:34:28 +0530

    done with functions

--- functions/main.go
+++ functions/main.go
@@ -8,6 +8,24 @@ func add(x, y int) int {
 	return x + y
 }
 
+func mixed(x string, y int) (int, string) {
+	return y + 10, x + " added string"
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
 	fmt.Println(add(5, 5))
+	fmt.Println(mixed("hello", 5))
+	a, b := mixed("hello", 5)
+	fmt.Println(a)
+	fmt.Println(b)
+
+	x, y := split(100)
+	fmt.Println(x)
+	fmt.Println(y)
 }
