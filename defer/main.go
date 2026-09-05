8910fa952403fc57e783c62222d981e5e29a7804
Prathmesh Adhav <adhavprathmesh972@gmail.com>
2026-09-05 20:57:36 +0530

    stacking defers

--- defer/main.go
+++ defer/main.go
@@ -11,4 +11,15 @@ func main() {
 
 	//The deferred call's arguments are evaluated immediately,
 	//  but the function call is not executed until the surrounding function returns.
+
+	//Stacking defers
+	//Deferred function calls are pushed onto a stack.
+	// When a function returns, its deferred calls are executed in last-in-first-out order.
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
 }
