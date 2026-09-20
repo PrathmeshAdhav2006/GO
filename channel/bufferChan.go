5b54fd4b4b800ba58edea8c15615be92d2fc7237
Prathmesh Adhav <adhavprathmesh972@gmail.com>
2026-09-20 20:57:03 +0530

    buffer channel

--- channel/bufferChan.go
+++ channel/bufferChan.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+// This program demonstrates the use of buffered channels in Go. A buffered channel allows you to send
+// a limited number of values into the channel without requiring a corresponding receiver for each send operation.
+// In this example, we create a buffered channel with a capacity of 2, send two integers into it, and then
+// receive and print those integers.
 func main() {
 	ch := make(chan int, 2)
 	ch <- 1
