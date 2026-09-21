3ced291b40b3812c4ab40fe47c57d02c5901275e
Prathmesh Adhav <adhavprathmesh972@gmail.com>
2026-09-21 23:24:33 +0530

    .

--- methods/main.go
+++ methods/main.go
@@ -9,6 +9,10 @@ type Vertex struct {
 	X, Y float64
 }
 
+// The Abs method calculates the absolute value (magnitude) of a Vertex
+// instance using the Pythagorean theorem. It takes a Vertex as a receiver and
+// returns the square root of the sum of the squares of its X and Y coordinates.
+
 func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
