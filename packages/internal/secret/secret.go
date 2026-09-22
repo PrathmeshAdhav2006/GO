8e5c2efdecfd0a6d6a6836ed5dc40b671813b1b7
Prathmesh Adhav <adhavprathmesh972@gmail.com>
2026-09-22 22:46:33 +0530

    reveal()

--- packages/internal/secret/secret.go
+++ packages/internal/secret/secret.go
@@ -1,5 +1,6 @@
 package secret
 
+// Reveal is a function that returns a string message indicating that it is only importable from within the 'packages' module tree.
 func Reveal() string {
 	return "This is only importable from within the 'packages' module tree."
 }
